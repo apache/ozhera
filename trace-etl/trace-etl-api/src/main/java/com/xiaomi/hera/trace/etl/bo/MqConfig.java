f205a0c0ff2140ffae40b0c30f1d1b365b1b7a30
EricDing <128116675+sadadw1@users.noreply.github.com>
2023-09-22 11:19:26 +0800

    Rocketmq producer extension (#71)
    * update
    
    * RocketMQ Producer's extension
    
    * RocketMQ extension Merged into one

--- trace-etl/trace-etl-api/src/main/java/com/xiaomi/hera/trace/etl/bo/MqConfig.java
+++ trace-etl/trace-etl-api/src/main/java/com/xiaomi/hera/trace/etl/bo/MqConfig.java
@@ -3,20 +3,26 @@ package com.xiaomi.hera.trace.etl.bo;
 import lombok.Data;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author goodjava@qq.com
  * @date 2023/9/19 17:36
  */
 @Data
-public class MqConfig implements Serializable {
+public class MqConfig<T> implements Serializable {
 
-
-    private String group;
 
     private String nameSerAddr;
 
-    private String topicName;
+    private String consumerGroup;
+    private String consumerTopicName;
 
+    private String producerGroup;
+    private String producerTopicName;
+
+    private Function<T, Boolean> consumerMethod;
+    private Function<List<T>, Boolean> batchConsumerMethod;
 
 }
--- trace-etl/trace-etl-server/src/main/java/com/xiaomi/hera/trace/etl/consumer/ConsumerService.java
+++ trace-etl/trace-etl-server/src/main/java/com/xiaomi/hera/trace/etl/consumer/ConsumerService.java
@@ -1,10 +1,16 @@
 package com.xiaomi.hera.trace.etl.consumer;
 
 import com.alibaba.nacos.api.config.annotation.NacosValue;
-import com.xiaomi.hera.trace.etl.api.service.MqExtension;
+import com.xiaomi.hera.trace.etl.api.service.IEnterManager;
+import com.xiaomi.hera.trace.etl.api.service.IMetricsParseService;
+import com.xiaomi.hera.trace.etl.api.service.MQExtension;
 import com.xiaomi.hera.trace.etl.bo.MqConfig;
+import com.xiaomi.hera.trace.etl.util.ThriftUtil;
+import com.xiaomi.hera.tspandata.TSpanData;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+import org.apache.thrift.TDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import run.mone.docean.spring.extension.Extensions;
@@ -22,7 +28,10 @@ import javax.annotation.Resource;
 public class ConsumerService {
 
     @Value("${mq.rocketmq.consumer.group}")
-    private String group;
+    private String consumerGroup;
+
+    @Value("${mq.rocketmq.producer.group}")
+    private String producerGroup;
 
     @NacosValue("${mq.rocketmq.nameseraddr}")
     private String nameSerAddr;
@@ -30,18 +39,53 @@ public class ConsumerService {
     @Value("${mq.rocketmq.server.topic}")
     private String topicName;
 
+    @Value("${mq.rocketmq.es.topic}")
+    private String esTopicName;
+
+    @Resource
+    private IEnterManager enterManager;
+
+    @Resource
+    private IMetricsParseService metricsExporterService;
 
     @Resource
     private Extensions extensions;
 
     @PostConstruct
     public void takeMessage() throws MQClientException {
-        MqExtension extension = extensions.get("mq");
-        MqConfig config = new MqConfig();
-        config.setGroup(group);
+        MQExtension<MessageExt> mq = extensions.get("mq");
+
+        MqConfig<MessageExt> config = new MqConfig<>();
         config.setNameSerAddr(nameSerAddr);
-        config.setTopicName(topicName);
-        extension.initMq(config);
+        config.setProducerGroup(producerGroup);
+        config.setProducerTopicName(esTopicName);
+
+        config.setConsumerGroup(consumerGroup);
+        config.setConsumerTopicName(topicName);
+
+        config.setBatchConsumerMethod((list)->{
+            enterManager.enter();
+            enterManager.getProcessNum().incrementAndGet();
+            try {
+                for (MessageExt message : list) {
+                    String traceId = "";
+                    try {
+                        TSpanData tSpanData = new TSpanData();
+                        new TDeserializer(ThriftUtil.PROTOCOL_FACTORY).deserialize(tSpanData, message.getBody());
+                        traceId = tSpanData.getTraceId();
+                        metricsExporterService.parse(tSpanData);
+                    } catch (Throwable t) {
+                        log.error("consumer message error", t);
+                    }
+                    mq.sendByTraceId(traceId, message);
+                }
+                return true;
+            } finally {
+                enterManager.getProcessNum().decrementAndGet();
+            }
+        });
+
+        mq.initMq(config);
     }
 
 

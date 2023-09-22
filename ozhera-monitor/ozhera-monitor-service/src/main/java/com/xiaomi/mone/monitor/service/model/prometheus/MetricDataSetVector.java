bcce96dc84f7b6361898684950bd6196a1b33353
gaoxihui <gaoxihui@xiaomi.com>
2023-09-22 11:01:25 +0800

    update annotation

--- ozhera-monitor/ozhera-monitor-service/src/main/java/com/xiaomi/mone/monitor/service/model/prometheus/MetricDataSetVector.java
+++ ozhera-monitor/ozhera-monitor-service/src/main/java/com/xiaomi/mone/monitor/service/model/prometheus/MetricDataSetVector.java
@@ -8,11 +8,10 @@ import java.util.List;
 /**
  * @author gaoxihui
  * @date 2021/8/26 8:51 下午
- * 瞬时向量查询数据模型，只有一个value
+ * Instantaneous vector query data model with only one value
  */
 @Data
 public class MetricDataSetVector implements Serializable {
     private Metric metric;
     private List<String> value;
-    private List<String> values;
 }

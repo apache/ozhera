a88aebb94c583c2764903840a86701a876ed5101
wodiwudi <40229449+wodiwudi@users.noreply.github.com>
2024-01-15 10:32:58 +0800

    refactor: update prometheus-agent dependency (#253)

--- ozhera-prometheus-agent/ozhera-prometheus-agent-service/src/main/java/com/xiaomi/youpin/prometheus/agent/service/alarmContact/FeishuAlertContact.java
+++ ozhera-prometheus-agent/ozhera-prometheus-agent-service/src/main/java/com/xiaomi/youpin/prometheus/agent/service/alarmContact/FeishuAlertContact.java
@@ -8,10 +8,8 @@ import com.xiaomi.youpin.prometheus.agent.result.alertManager.Alerts;
 import com.xiaomi.youpin.prometheus.agent.result.alertManager.CommonLabels;
 import com.xiaomi.youpin.prometheus.agent.result.alertManager.GroupLabels;
 import com.xiaomi.youpin.prometheus.agent.service.FeishuService;
-import com.xiaomi.youpin.prometheus.agent.util.DateUtil;
 import com.xiaomi.youpin.prometheus.agent.util.FreeMarkerUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 

e3e2f7c10f64bb2cb4feb31c82c4b84d24cbd050
wtt <1136220284@qq.com>
2024-07-12 10:39:59 +0800

    refactor: add header

--- ozhera-log/log-agent/src/test/java/com/xiaomi/mone/log/agent/common/ExecutorUtilTest.java
+++ ozhera-log/log-agent/src/test/java/com/xiaomi/mone/log/agent/common/ExecutorUtilTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Xiaomi
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.xiaomi.mone.log.agent.common;
 
 import junit.framework.TestCase;
@@ -20,7 +35,7 @@ public class ExecutorUtilTest extends TestCase {
 
         ExecutorUtil.scheduleAtFixedRate(() -> {
             log.info("I am a task");
-        }, 10, 30, java.util.concurrent.TimeUnit.SECONDS);
+        }, 10, 10, java.util.concurrent.TimeUnit.SECONDS);
 
         ExecutorUtil.submit(() -> {
             while(true){
@@ -33,7 +48,7 @@ public class ExecutorUtilTest extends TestCase {
             }
         });
 
-        Thread.sleep(TimeUnit.MINUTES.toMillis(2));
+        Thread.sleep(TimeUnit.MINUTES.toMillis(1));
     }
 
 }

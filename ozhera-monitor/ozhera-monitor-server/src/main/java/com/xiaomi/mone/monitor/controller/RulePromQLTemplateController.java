e130be6a48fd101b08975efe0f966acada4301b1
gaoxh <32359336+gaoxh@users.noreply.github.com>
2024-08-14 18:48:24 +0800

    fix: add header (#408)
    * fix: add header
    
    * fix: add header
    
    ---------
    
    Co-authored-by: gaoxihui <gaoxihui@xiaomi.com>

--- ozhera-monitor/ozhera-monitor-server/src/main/java/com/xiaomi/mone/monitor/controller/RulePromQLTemplateController.java
+++ ozhera-monitor/ozhera-monitor-server/src/main/java/com/xiaomi/mone/monitor/controller/RulePromQLTemplateController.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2020 Xiaomi Corporation
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package com.xiaomi.mone.monitor.controller;
 
 import com.xiaomi.mone.monitor.bo.RulePromQLTemplateInfo;
@@ -48,7 +63,7 @@ public class RulePromQLTemplateController {
             log.info("RulePromQLTemplateController.add param : {} ,user : {}", param, user);
             return rulePromQLTemplateService.add(user, param);
         } catch (Exception e) {
-            log.error("RulePromQLTemplateController.add异常 param : {} ,userInfo :{}", param, userInfo, e);
+            log.error("RulePromQLTemplateController.add exception! param : {} ,userInfo :{}", param, userInfo, e);
             return Result.fail(ErrorCode.unknownError);
         }
     }
@@ -70,7 +85,7 @@ public class RulePromQLTemplateController {
             log.info("RulePromQLTemplateController.edit param : {} ,user : {}", param, user);
             return rulePromQLTemplateService.edit(user, param);
         } catch (Exception e) {
-            log.error("RulePromQLTemplateController.edit异常 param : {} ,userInfo :{}", param, userInfo, e);
+            log.error("RulePromQLTemplateController.edit exception! param : {} ,userInfo :{}", param, userInfo, e);
             return Result.fail(ErrorCode.unknownError);
         }
     }
@@ -92,7 +107,7 @@ public class RulePromQLTemplateController {
             log.info("RulePromQLTemplateController.deleteById param : {} ,user : {}", param, user);
             return rulePromQLTemplateService.deleteById(user, param.getId());
         } catch (Exception e) {
-            log.error("RulePromQLTemplateController.deleteById异常 param : {} ,userInfo :{}", param, userInfo, e);
+            log.error("RulePromQLTemplateController.deleteById exception! param : {} ,userInfo :{}", param, userInfo, e);
             return Result.fail(ErrorCode.unknownError);
         }
     }
@@ -112,7 +127,7 @@ public class RulePromQLTemplateController {
             log.info("AlarmStrategyController.search param : {} ,user : {}", param, user);
             return rulePromQLTemplateService.search(user, param);
         } catch (Exception e) {
-            log.error("AlarmStrategyController.search异常 param : {} ,userInfo :{}", param, userInfo, e);
+            log.error("AlarmStrategyController.search exception! param : {} ,userInfo :{}", param, userInfo, e);
             return Result.fail(ErrorCode.unknownError);
         }
     }
@@ -134,7 +149,7 @@ public class RulePromQLTemplateController {
             log.info("AlarmStrategyController.testPromQL param : {} ,user : {}", param, user);
             return rulePromQLTemplateService.testPromQL(user, param);
         } catch (Exception e) {
-            log.error("AlarmStrategyController.testPromQL异常 param : {} ,userInfo :{}", param, userInfo, e);
+            log.error("AlarmStrategyController.testPromQL exception! param : {} ,userInfo :{}", param, userInfo, e);
             return Result.fail(ErrorCode.unknownError);
         }
     }

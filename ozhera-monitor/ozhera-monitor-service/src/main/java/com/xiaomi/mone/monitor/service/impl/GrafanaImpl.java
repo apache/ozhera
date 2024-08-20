12c1c149783e1bd38609d87ae3e056d8166fc6f5
gaoxihui <gaoxihui@xiaomi.com>
2024-08-20 15:21:00 +0800

    fix: add header and remove unused class files

--- ozhera-monitor/ozhera-monitor-service/src/main/java/com/xiaomi/mone/monitor/service/impl/GrafanaImpl.java
+++ ozhera-monitor/ozhera-monitor-service/src/main/java/com/xiaomi/mone/monitor/service/impl/GrafanaImpl.java
@@ -1,12 +1,25 @@
+/*
+ *  Copyright (C) 2020 Xiaomi Corporation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.xiaomi.mone.monitor.service.impl;
 
-import com.xiaomi.mone.monitor.service.GrafanaApiService;
+import com.xiaomi.mone.monitor.service.Grafana;
 import com.xiaomi.mone.monitor.service.GrafanaService;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.apache.dubbo.config.annotation.Service;
-
-import com.xiaomi.mone.monitor.service.Grafana;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @author wodiwudi
--- ozhera-monitor/ozhera-monitor-service/src/main/java/com/xiaomi/mone/monitor/service/model/prometheus/AlarmRuleDataRemote.java
+++ ozhera-monitor/ozhera-monitor-service/src/main/java/com/xiaomi/mone/monitor/service/model/prometheus/AlarmRuleDataRemote.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright (C) 2020 Xiaomi Corporation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package com.xiaomi.mone.monitor.service.model.prometheus;
 
 import lombok.Data;
@@ -8,24 +23,24 @@ import java.util.Map;
 
 /**
  * @author gaoxihui
- * @date 2021/9/17 2:25 下午
+ * @date 2021/9/17 2:25 PM
  */
 @Data
 public class AlarmRuleDataRemote implements Serializable {
 
 
     private Integer id;//int	alert id
-    private String cname;//	alert 别名
+    private String cname;//	alert Aliases
     private String alert;//	alert name
-    private String  expr;//	表达式
+    private String  expr;//	expression
     private Map labels;//	map	labels
-    private String annotations	;//	告警描述信息
+    private String annotations	;//	Alarm description
     private String group	;//	group name
-    private String env	;//	配置环境
-    Boolean enabled;// bool	是否启用
-    private String priority	;//	告警级别
-    List<AlertTeamData> alert_team;//	map	告警组
-    private String created_by	;//	创建人
-    Long created_time;//	timestamp	创建时间
+    private String env	;//	Configure the environment
+    Boolean enabled;// bool	Is it enabled
+    private String priority	;//	Alarm level
+    List<AlertTeamData> alert_team;// Alarm Groups
+    private String created_by	;//	creator
+    Long created_time;
 
 }

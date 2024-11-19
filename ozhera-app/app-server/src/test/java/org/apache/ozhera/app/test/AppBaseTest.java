6ef901fcb6f48e2cb4c270ceb0292bdb96498980
gaoxihui <gaoxihui@xiaomi.com>
2024-11-19 19:48:36 +0800

    fix: update the copyright statement in the file header and add a notice file

--- ozhera-app/app-server/src/test/java/org/apache/ozhera/app/test/AppBaseTest.java
+++ ozhera-app/app-server/src/test/java/org/apache/ozhera/app/test/AppBaseTest.java
@@ -1,27 +1,30 @@
 /*
- * Copyright (C) 2020 Xiaomi Corporation
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
-//package com.xiaomi.mone.test;
+//package org.apache.ozhera.app.test;
 //
 //import com.google.gson.Gson;
-//import com.xiaomi.mone.app.api.model.HeraAppBaseInfoModel;
-//import com.xiaomi.mone.app.api.model.HeraAppBaseInfoParticipant;
-//import com.xiaomi.mone.app.api.model.HeraAppBaseQuery;
-//import com.xiaomi.mone.app.AppBootstrap;
-//import com.xiaomi.mone.app.service.impl.HeraAppBaseInfoService;
 //import lombok.extern.slf4j.Slf4j;
+//import org.apache.ozhera.app.AppBootstrap;
+//import org.apache.ozhera.app.api.model.HeraAppBaseInfoModel;
+//import org.apache.ozhera.app.api.model.HeraAppBaseInfoParticipant;
+//import org.apache.ozhera.app.api.model.HeraAppBaseQuery;
+//import org.apache.ozhera.app.service.impl.HeraAppBaseInfoService;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.context.SpringBootTest;
--- ozhera-app/app-server/src/test/java/org/apache/ozhera/app/test/HeraProjectGroupTest.java
+++ ozhera-app/app-server/src/test/java/org/apache/ozhera/app/test/HeraProjectGroupTest.java
@@ -1,17 +1,20 @@
 /*
- * Copyright (C) 2020 Xiaomi Corporation
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 //package com.xiaomi.mone.test;
 //
@@ -19,22 +22,20 @@
 //import com.alibaba.nacos.api.config.annotation.NacosValue;
 //import com.alibaba.nacos.client.config.utils.MD5;
 //import com.google.gson.Gson;
-//import com.xiaomi.mone.app.AppBootstrap;
-//import com.xiaomi.mone.app.api.model.HeraAppBaseInfoModel;
-//import com.xiaomi.mone.app.api.model.HeraAppBaseInfoParticipant;
-//import com.xiaomi.mone.app.api.model.HeraAppBaseQuery;
-//import com.xiaomi.mone.app.api.model.project.group.HeraProjectGroupDataRequest;
-//import com.xiaomi.mone.app.api.model.project.group.ProjectGroupTreeNode;
-//import com.xiaomi.mone.app.auth.AuthorizationService;
-//import com.xiaomi.mone.app.common.Result;
-//import com.xiaomi.mone.app.service.impl.HeraAppBaseInfoService;
-//import com.xiaomi.mone.app.service.project.group.HeraProjectGroupService;
 //import lombok.extern.slf4j.Slf4j;
+//import org.apache.ozhera.app.AppBootstrap;
+//import org.apache.ozhera.app.api.model.HeraAppBaseInfoModel;
+//import org.apache.ozhera.app.api.model.project.group.HeraProjectGroupDataRequest;
+//import org.apache.ozhera.app.api.model.project.group.ProjectGroupTreeNode;
+//import org.apache.ozhera.app.auth.AuthorizationService;
+//import org.apache.ozhera.app.common.Result;
+//import org.apache.ozhera.app.service.project.group.HeraProjectGroupService;
 //import org.junit.jupiter.api.Test;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.context.SpringBootTest;
 //
 //import java.util.List;
+//
 //
 ///**
 // * @author gaoxihui

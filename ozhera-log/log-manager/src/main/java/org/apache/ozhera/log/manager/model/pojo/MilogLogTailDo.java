90f679ecd17feb1a5f54fb1ae144ca783a6520f0
xs3508198 <1131259489@qq.com>
2025-02-24 14:22:07 +0800

    feat: add log level filtering for collection
    - Implements a new feature to filter log collection based on log levels.
    - Allows users to specify the desired log levels for collection, enhancing the system's flexibility and user experience.
    - The implementation includes a configuration option to specify the desired log levels and corresponding filtering logic in the log collection module.

--- ozhera-log/log-manager/src/main/java/org/apache/ozhera/log/manager/model/pojo/MilogLogTailDo.java
+++ ozhera-log/log-manager/src/main/java/org/apache/ozhera/log/manager/model/pojo/MilogLogTailDo.java
@@ -162,6 +162,13 @@ public class MilogLogTailDo extends BaseCommon {
     @Comment("Indicates the system that originated the record; 'hera' for internal, and others for external sources.")
     private String originSystem;
 
+    @Column(value = "collected_log_levels")
+    @ColDefine(type = ColType.MYSQL_JSON)
+    @Comment("Level list of logs to be collected.")
+    @JSONField
+    private List<String> CollectedLogLevelList;
+
+
     public MilogLogTailDo() {
     }
 
